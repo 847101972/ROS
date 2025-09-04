deb4f427fe3a7ca56e02aa661442761d18fcdbf0
847101972 <847101972@qq.com>
2025-09-05 00:20:06 +0800

    Firstsave

---
+++ ssr_pkg/src/yao_node.cpp
@@ -0,0 +1,21 @@
+#include <ros/ros.h>
+#include<std_msgs/String.h>
+
+int main(int argc, char  *argv[])
+{
+    ros::init(argc,argv,"yao_node");
+    printf("Hello world");
+    ros::NodeHandle nh;
+    ros::Publisher pub = nh.advertise<std_msgs::String>("GYM",10);
+    ros::Rate loop_rate(10);
+
+    while(ros::ok())
+    {
+        printf("1\n");
+        std_msgs::String msg;
+        msg.data = "生平长进，皆在荣辱之中";
+        pub.publish(msg);
+        loop_rate.sleep();
+    }
+    return 0;
+}
